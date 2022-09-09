504a72e84eecf5274e3c14b4e7564a3c6ad5f469
oyindamola richard <oyediranrichardoyindamola@gmail.com>
2022-09-09 22:35:28 +0100

    wrote js for functions

--- api.js
+++ api.js
@@ -1,13 +1,39 @@
 
 async function test (){
-    // let key = "55ae2e99fca0dd8d02b79e20bcb13981"
-    // let inte = inpt.value
-    const endpoint = `https://api.openweathermap.org/data/2.5/weather?q=${userLocation.value}&appid=55ae2e99fca0dd8d02b79e20bcb13981`
+    let key = "55ae2e99fca0dd8d02b79e20bcb13981"
+    const endpoint = `https://api.openweathermap.org/data/2.5/weather?q=${userLocation.value}&appid=${key}`
     let response = await fetch(endpoint)
     let jsonResponse = await response.json()
     console.log(jsonResponse)
 
-    disp.innerHTML =`
-        <h1>${jsonResponse.name}</h1>
-        `
+    cityName.innerHTML = jsonResponse.name;
+    degree.innerHTML = jsonResponse.main.temp
+    condition.innerHTML = jsonResponse.weather[0].description;
+    country.innerHTML = jsonResponse.sys.country
+    longitude.innerHTML = jsonResponse.coord.lon
+    latitude.innerHTML = jsonResponse.coord.lat
+    cloud.innerHTML = `${jsonResponse.clouds.all}${"%"}`
+    pressure.innerHTML = jsonResponse.main.pressure
+    humidity.innerHTML = `${jsonResponse.main.humidity}${"%"}`
+}
+
+const timeDisp = ()=>{
+    const time = new Date();
+
+    const hour = timeFormat((time.getHours()))
+    const minutes = timeFormat((time.getMinutes()))
+    const day = timeFormat((time.getDay()))
+    const month = timeFormat((time.getMonth()))
+    const year = timeFormat((time.getFullYear()))
+    clock.innerHTML = `${hour}:${minutes}-${day},${month} ${year}`
+}
+setInterval(timeDisp, 1000);
+
+const timeFormat = (timer)=>{
+    if(timer<10){
+    return '0'+timer
+    }
+    else{
+        return timer
+    }
 }
