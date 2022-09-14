a36e8cbca35cf248f0f03194e4b8b7e136afd80e
oyindamola richard <oyediranrichardoyindamola@gmail.com>
2022-09-14 16:19:37 +0100

    added api key back for use

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
 
 async function search (){
-    let key = "Your API Key..."
+    let key = "55ae2e99fca0dd8d02b79e20bcb13981"
     const endpoint = `https://api.openweathermap.org/data/2.5/weather?q=${userLocation.value}&appid=${key}&units=metric`
     let response = await fetch(endpoint)
     let jsonResponse = await response.json()
